ccf46eb623d44ada55d905ff900bab6405a59260
Thanapolseekhot <52207987+Thanapolseekhot@users.noreply.github.com>
2021-01-13 13:26:43 +0700

    Add files via upload

---
+++ grade.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+void main()
+{ 
+	cout<<"\n";
+	cout<<" 1st  Term of 2020"<<"\t" <<"\\6206021630014  Thanapol seekhot\n";
+	cout<<"\n";
+	cout<<"-------------------------------------------------------------------------------------\n";
+	cout<<"|   Subject ID  | Subject name"<<"\t\t   "<<"| Score (100)|  Grad  |\n";
+	cout<<"-------------------------------------------------------------------------------------\n";
+	cout<<"|    4570034    | Introduction to computer "<<"|     85     |"<<"   \"A\"  |\n";
+	cout<<"|    4580247    | English                  "<<"|     74     |"<<"   \"B\"  |\n";
+	cout<<"|    4530001    | Design Thinking          "<<"|     92     |"<<"   \"A\"  |\n";
+	cout<<"-------------------------------------------------------------------------------------\n";
+	cout<<"|     TOTAL     | Subject = 3              "<<"| Average = 3.87  |\n";
+	cout<<"==========================================\n";
+	cout<<"\n";cout<<"\n";cout<<"\n";
+	cout<<"KMUTNB\n";
+	getch();
+	
+}
